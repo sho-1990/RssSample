ced504ef74fcfaaf948320d4cc2004255b8d92db
shosato <shosato@MBP.local>
2017-07-14 01:25:17 +0900

    [add]Rss表示クラスの雛形と簡単な表示確認を追加

--- RssSample/RssSamplePage.xaml.cs
+++ RssSample/RssSamplePage.xaml.cs
@@ -1,12 +1,14 @@
-﻿using Xamarin.Forms;
+﻿using System.Collections.ObjectModel;
+using RssSample.entity;
+using Xamarin.Forms;
 
-namespace RssSample
-{
-    public partial class RssSamplePage : ContentPage
-    {
-        public RssSamplePage()
-        {
+namespace RssSample {
+    public partial class RssSamplePage : ContentPage {
+        ObservableCollection<Rss> rssDatas = new ObservableCollection<Rss>();
+        public RssSamplePage() {
             InitializeComponent();
+            list.ItemsSource = rssDatas;
+            rssDatas.Add(new Rss() { title = "title"});
         }
     }
 }
---
+++ RssSample/entity/Rss.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace RssSample.entity {
+    public class Rss {
+        public string title {
+            get;
+            set;
+        }
+        public Rss(){
+            this.title = "";
+        }
+    }
+}
