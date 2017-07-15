f22f520aa5f4013f0e066b07f80a55f5701c756d
shosato <shosato@MBP.local>
2017-07-15 12:22:44 +0900

    [update]Rssの戻りに合わせてrssクラスを修正

--- RssSample/entity/Rss.cs
+++ RssSample/entity/Rss.cs
@@ -5,6 +5,33 @@ namespace RssSample.entity {
             get;
             set;
         }
+        public string link {
+            get;
+            set;
+        }
+        public string pubDate {
+            get;
+            set;
+        }
+        public string enclosure {
+            get;
+            set;
+        }
+        public string guide {
+            get;
+            set;
+        }
+
+        public class Enclosure {
+            public string url {
+                get;
+                set;
+            }
+            public string type {
+                get;
+                set;
+            }
+        }
         public Rss(){
             this.title = "";
         }
