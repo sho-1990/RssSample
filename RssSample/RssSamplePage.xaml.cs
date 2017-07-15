40914e834c14db98b3f348cbf6d61e04cde3e639
shosato <shosato@MBP.local>
2017-07-16 00:26:19 +0900

    [update]試しに取ってきてログに出す

--- RssSample/RssSamplePage.xaml.cs
+++ RssSample/RssSamplePage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using RssSample.api;
 using RssSample.entity;
 using Xamarin.Forms;
 
@@ -7,8 +9,20 @@ namespace RssSample {
         ObservableCollection<Rss> rssDatas = new ObservableCollection<Rss>();
         public RssSamplePage() {
             InitializeComponent();
+            call();
+        }
+
+        private async void call() {
+			rssDatas = await ApiRss.Get();
+            foreach(Rss r in rssDatas) {
+                Debug.WriteLine(r.title);
+                Debug.WriteLine(r.link);
+                Debug.WriteLine(r.pubDate);
+                Debug.WriteLine(r.enclosureUrl);
+                Debug.WriteLine(r.enclosureType);
+            }
+
             list.ItemsSource = rssDatas;
-            rssDatas.Add(new Rss() { title = "title"});
         }
     }
 }
